458114b8206d285ddfc0be9678554687c43ba54a
sis111su <sis111su@3e14af01-25bf-4646-b2ef-b042f07ede10>
2015-11-09 12:37:16 +0000

    Updated ignore list

--- linefollower/rxeflash.sh
+++
@@ -1,4 +0,0 @@
-echo Executing NeXTTool to upload LineFollower.rxe...
- /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -download=LineFollower.rxe
- /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -listfiles=LineFollower.rxe
-echo NeXTTool is terminated.
